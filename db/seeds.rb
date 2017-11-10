5fb9b1053810fbc459d937db7d038a8bc3519697
webapp <webapp@test.com>
2017-11-10 16:56:42 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = User.create(
+    [
+      { username: 'Admin', email: 'admin@example.com'},
+      { username: 'Czarek',email: 'czarek@example.org'}
+    ]
+)
+
 
 articles = Article.create(
     [
         {title: 'First article',
-         description: 'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id. Vivamus non odio sit amet metus scelerisque.'
-        },
+         description:
+                'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id.
+Vivamus non odio sit amet metus scelerisque.',
+         user_id: 1 },
         {title: 'Second article',
-         description: 'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id. Vivamus non odio sit amet metus scelerisque.'
-        },
+        description:
+                'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id.
+Vivamus non odio sit amet metus scelerisque.',
+         user_id: 1 },
         {title: 'Third article',
-         description: 'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id. Vivamus non odio sit amet metus scelerisque.'
-        },
+         description:
+                'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id.
+Vivamus non odio sit amet metus scelerisque.',
+        user_id: 2},
         {title: 'Test',
-        description: 'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id. Vivamus non odio sit amet metus scelerisque.'
-        }
+         description: 'Sed non scelerisque lorem. Aliquam ultrices elit ante, eget tincidunt est sagittis id.
+Vivamus non odio sit amet metus scelerisque.',
+         user_id: 2}
     ]
 )
